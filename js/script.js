388e29231699b223fcf86e03b0b8cb878efb0d75
LukasCastillo <112668532+LukasCastillo@users.noreply.github.com>
2022-10-31 16:35:40 +0800

    Responsive header

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
+//adds a margin to the selectors based on the width of the menu
+function fixHeader(){
+  let dropdowns = document.getElementsByClassName("nav-dropdown-content");
+  if(window.innerWidth <= 560){
+    let width = document.getElementById("nav-buttons").clientWidth;
+    for(let i = 0; i < dropdowns.length; i++) dropdowns[i].style.marginLeft = width + "px";
+  }
+  else{ 
+    for(let i = 0; i < dropdowns.length; i++) dropdowns[i].style.marginLeft = "";
+  }
+}
+
 window.onload = function() {
+  //toggles header button
+  document.getElementById("menu-button").onclick = function() {
+    let nav = document.getElementById("nav-buttons");
+    if(window.getComputedStyle(nav).display == "block") nav.style.display = "";
+    else nav.style.display = "block";
+    fixHeader();
+  }
+  
   //removes transition lock
   //https://stackoverflow.com/questions/22222810/disable-css-transitions-on-page-load-only
   document.body.classList.remove("preload");
@@ -8,4 +28,8 @@ window.onload = function() {
       func();
     }
   }
+}
+
+window.onresize = function() {
+  fixHeader();
 }
